b9f6798b4ddc52bfbd2a9e1b7d3d65cdd24a95b5
tfrank0651 <tfrank0651@gmail.com>
2022-08-14 19:54:30 -0400

    typo fix

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -25,7 +25,7 @@ def register():
         if error is None:
             try:
                 db.execute(
-                    'INSERT INTO user (username, password) VALUES (?,?',
+                    "INSERT INTO user (username, password) VALUES (?, ?)",
                     (username, generate_password_hash(password))
                 )
                 db.commit()
